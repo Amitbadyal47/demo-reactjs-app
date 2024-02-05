21b7307a9d6de4b3c2dadef29db13ca898242bd4
Amitbadyal47 <103357216+Amitbadyal47@users.noreply.github.com>
2024-02-05 14:18:59 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Jenkins!</h1>
+        <p>This is a simple ReactJS app deployed with Jenkins.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
